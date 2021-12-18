577e79fe473f56828c48f6fef3234a823511f66a
Роман Красиков <krasikov100699@gmail.com>
2021-12-18 19:08:05 +0300

    Класс снаряжения

---
+++ Equipment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsEquipCatalog
+{
+    public class Equipment
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Id { get; set; }
+        public bool Available { get; set; }
+
+        public Equipment(string Name, double Price, int Id, bool Available)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.Id = Id;
+            this.Available = Available;
+        }
+    }
+}

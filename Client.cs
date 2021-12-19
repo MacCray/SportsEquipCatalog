e68589f1818ce0a834fb89c8e4c74a31a40d5a61
Роман Красиков <krasikov100699@gmail.com>
2021-12-19 12:55:14 +0300

    Добавлен класс покупателя

---
+++ Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsEquipCatalog
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public bool IsRegular { get; set; }
+        public Client(string Name, string Number, bool IsRegular)
+        {
+            this.Name = Name;
+            this.Number = Number;
+            this.IsRegular = IsRegular;
+        }
+    }
+}

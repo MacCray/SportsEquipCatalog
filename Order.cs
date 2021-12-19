dde1ed8a52100f00a17aced83c0ae9dbe51c9fff
Роман Красиков <krasikov100699@gmail.com>
2021-12-19 12:56:03 +0300

    Добавлен класс заказа

---
+++ Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsEquipCatalog
+{
+    public class Order
+    {
+        public List<Equipment> equipmentForRent = new();
+        public double TotalPrice { get; set; }
+        public int OrderTime { get; set; }
+        public Client client { get; set; }
+
+        public Order(List<Equipment> equipmentForRent,int OrderTime,Client client)
+        {
+            this.equipmentForRent = equipmentForRent;
+            this.OrderTime = OrderTime;
+            this.client = client;
+            CalculateTotalPrice();
+        }
+        void CalculateTotalPrice()
+        {
+            equipmentForRent.ForEach(equ => TotalPrice += equ.Price);
+            if (OrderTime >= 5 || client.IsRegular)
+            {
+                TotalPrice *= 0.95;
+            }
+        }
+    }
+}

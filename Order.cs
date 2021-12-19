0a91668d439eabcb80613edb197d63457fa4b38a
Роман Красиков <krasikov100699@gmail.com>
2021-12-19 19:06:15 +0300

    Класс заказа

--- Order.cs
+++ Order.cs
@@ -10,6 +10,7 @@ namespace SportsEquipCatalog
     {
         public List<Equipment> equipmentForRent = new();
         public double TotalPrice { get; set; }
+        public double Pledge { get; set; }
         public int OrderTime { get; set; }
         public Client client { get; set; }
 
@@ -19,11 +20,12 @@ namespace SportsEquipCatalog
             this.OrderTime = OrderTime;
             this.client = client;
             CalculateTotalPrice();
+            Pledge = TotalPrice * 5;
         }
         void CalculateTotalPrice()
         {
             equipmentForRent.ForEach(equ => TotalPrice += equ.Price);
-            if (OrderTime >= 5 || client.IsRegular)
+            if (OrderTime >= 7 || client.IsRegular)
             {
                 TotalPrice *= 0.95;
             }
